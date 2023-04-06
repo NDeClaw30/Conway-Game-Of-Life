081bb6a34ba16062b3f25380de1a6f9abb8c3d07
calebtoberman <tobermancaleb@gmail.com>
2023-04-06 09:45:21 -0500

    Merged Nehemiah's code into mine.

--- Main.py
+++ Main.py
@@ -1,16 +1,30 @@
 import sys,pygame
-from view import GoLView
-from model import GoLModel
+from View import GoLView
+from Model import GoLModel
 def rungame():
     pygame.init()
     view = GoLView()
     model = GoLModel(view.displaywidth // view.blockSize, view.displayheight // view.blockSize)
+    x = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        view.update()
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                  
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if view.b2.collidepoint(pygame.mouse.get_pos()) and x == 1:
+                    x = 2
+                    view.quit()
+                    pygame.quit()
+                elif view.b1.collidepoint(pygame.mouse.get_pos()) and x == 0:
+                    x = x + 1
+                    view.start()
+        view.update(x)
         
 
 
--- View.py
+++ View.py
@@ -1,5 +1,17 @@
 import pygame
 
+def button(screen, position, text):
+            font = pygame.font.SysFont("Arial", 50)
+            text_render = font.render(text, 1, (255, 0, 0))
+            x, y, w , h = text_render.get_rect()
+            x, y = position
+            pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w , y), 5)
+            pygame.draw.line(screen, (150, 150, 150), (x, y - 2), (x, y + h), 5)
+            pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w , y + h), 5)
+            pygame.draw.line(screen, (50, 50, 50), (x + w , y+h), [x + w , y], 5)
+            pygame.draw.rect(screen, (100, 100, 100), (x, y, w , h))
+            return screen.blit(text_render, (x, y))
+
 class GoLView:
 
     def __init__(self):
@@ -9,6 +21,8 @@ class GoLView:
         self.displayheight = 720
         self.blockSize = 20
         self._DS = pygame.display.set_mode((self.width,self.height))
+        self.b1 = button(self._DS, (self.width // 2, self.height // 2), "Start")
+        self.b2 = button(self._DS, (self.width // 2, self.height //2), "Quit")
     
     def draw_grid(self):
         for x in range(0, self.displaywidth, self.blockSize):
@@ -16,9 +30,24 @@ class GoLView:
             rect = pygame.Rect(x, y, self.blockSize, self.blockSize)
             pygame.draw.rect(self._DS, (0,0,0), rect, 1)
 
-    def update(self):
-        self._DS.fill((220,220,220))
-        pygame.draw.line(self._DS, (0,0,0), (self.displaywidth,0),(self.displaywidth,self.displayheight))
-        self.draw_grid()
-        pygame.display.update()
+
+    def start(self):
+        self._DS.fill("white")
+        print("Ok, let's go")
+        pygame.draw.circle(self._DS, (0 , 0 , 0), (250,50) , 100 )
+
+    def quit(self):
+        self._DS.fill("Black")
+        print("Thanks for Playing")
+
+    def update(self, x):
+        if x == 0:
+            self.b1 = button(self._DS, (self.width // 2, self.height // 2), "Start")
+            pygame.display.update()
+        if x == 1:
+            self._DS.fill((220,220,220))
+            pygame.draw.line(self._DS, (0,0,0), (self.displaywidth,0),(self.displaywidth,self.displayheight))
+            self.draw_grid()
+            self.b2 = button(self._DS, (1325,360), "Quit")
+            pygame.display.update()
     
