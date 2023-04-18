e6eefa83564482d63d6881729fed10b87215c6d8
NDeClaw30 <122389048+NDeClaw30@users.noreply.github.com>
2023-04-18 02:45:48 -0500

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,1 +1,16 @@
+import pygame
+from Model import GoLModel
+from View import GoLView
 
+class GoLController:
+
+    def __init__(self, view, model):
+        self._view = view
+        self._model = model
+        self.running = False
+
+    def toggleCell(self, click):
+        if click.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.getPos()
+            if pygame.Rect(0,0, self._view.displaywidth, self._view.displayheight).collidepoint(pos):
+                pass
