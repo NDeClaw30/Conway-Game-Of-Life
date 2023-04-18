e729a4690f25e5777ae3923a7be2620de42f0cff
NDeClaw30 <122389048+NDeClaw30@users.noreply.github.com>
2023-04-18 02:46:44 -0500

    Update Model.py

--- Model.py
+++ Model.py
@@ -8,7 +8,7 @@ class GoLModel:
         self._height = h + 2
         self._grid = [[0] * self._width for i in range(self._height)]
         self.randomCell()
-    
+
 
     def update(self):
         pass    
@@ -30,7 +30,7 @@ class GoLModel:
                 count += self._grid[j][i]
         count -= self._grid[y][x]
         return count
-    
+
     def DeadOrAlive(self):
         next = [[0] * self._width for i in range(self._height)]
         for x in range(1, self._width - 1):
@@ -42,15 +42,15 @@ class GoLModel:
                     next[y][x] = 1
                 elif self._grid[y][x] == 1 and 0<=count<=1:
                     next[y][x] = 0
-                    
+
         self._grid = next
-    
+
     def getSize(self):
         return self._width,self._height
 
     def getGrid(self):
         return [self._grid[i][1:-1] for i in range(1,self._height - 1)]
-    
+
     def toggle(self):
         pass
 
@@ -67,40 +67,3 @@ if __name__ == '__main__':
     m.DeadOrAlive()
     for r in m._grid:
         print(r)
-class Cell:
-    def __init__(self):
-        '''
-        Class holding init status of cell (dead).
-        Ability to set- and fetch new statuses with functions
-        '''
-        self._status = 'Dead'
-
-    def set_dead(self):
-        '''
-        method sets the cell status to DEAD
-        '''
-        self._status = 'Dead'
-
-    def set_alive(self):
-        '''
-        method sets the cell status to ALIVE
-        '''
-        self._status = 'Alive'
-
-    def is_alive(self):
-        '''
-        method checks if the cell is ALIVE
-        returns True if it is alive, False if not.
-        '''
-        if self._status == 'Alive':
-            return True
-        return False
-
-    def get_print_character(self):
-        '''
-        method returning a status character of our choice to print on the board
-        '''
-        if self.is_alive():
-            return 'O'
-        return '.'
-
